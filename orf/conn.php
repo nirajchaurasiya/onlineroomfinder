5e96411d110663ffed2239be501d92b015a9136f
Niraj Chaurasiya <developer.nirajchaurasiya@gmail.com>
2023-07-27 20:28:07 +0545

    updates

--- orf/conn.php
+++ orf/conn.php
@@ -1,8 +1,8 @@
 <?php 
-    $servername = "sql307.epizy.com";
-    $username = "epiz_31932881";
-    $password = "HQPIhRiZ0bu";
-    $db = "epiz_31932881_onlineroomfiner";
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db = "orf";
     $con = new mysqli($servername,$username,$password,$db);
     if(!$con)
     {
--- orf/index.php
+++ orf/index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="Welcome to iCoder. A blog for coding enthusiasts">
+  <!-- <meta name="description" content="Welcome to iCoder. A blog for coding enthusiasts"> -->
 
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/services.css">
@@ -12,7 +12,7 @@
 
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <link rel="stylesheet" href="css/index.css">
-  <link rel="stylesheet" href="css/lock.css">
+  <!-- <link rel="stylesheet" href="css/lock.css"> -->
   <title>Online Room Finders</title>
 </head>
 
